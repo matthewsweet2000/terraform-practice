c06a80fbef09112e8c2b23f914b283784292028d
Matt Sweet <matthewsweet2000@yahoo.co.uk>
2020-04-19 20:37:03 +0100

    Push script for docker image to AWS

--- warehouse/heat/report_1/scripts/docker-push.bat
+++ warehouse/heat/report_1/scripts/docker-push.bat
@@ -1,1 +1,15 @@
-docker build -t heat_report_1 ..\..\.. --file ..\..\..\docker\heat_report_1.dockerFile
+@ECHO off
+
+FOR /F %%A in (..\..\..\.env) do SET %%A
+
+SET IMAGE_NAME=heat_report_1
+
+SET DOCKER_TAG=%AWS_ACCOUNT_HEAT%.dkr.ecr.%AWS_REGION_HEAT%.amazonaws.com/%IMAGE_NAME%
+
+aws-mfa
+
+FOR /f "tokens=*" %%i in ('aws ecr get-login --no-include-email') DO SET LOGIN_CMD=%%i
+
+%LOGIN_CMD%
+
+docker push %DOCKER_TAG%
